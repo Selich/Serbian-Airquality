1694636b511652bb9d99aa27d56b02c48833432c
Nikola Selic <selich.work@gmail.com>
2020-01-06 01:00:25 +0100

    prediction works

--- algorithms/ridge.py
+++ algorithms/ridge.py
@@ -6,15 +6,15 @@ class Ridge:
 
     def __init__(self, lambda_=1.):
         self.lambda_ = lambda_  # λ
-        self.w_ = None
+        self.w = None
 
     def fit(self, X, t):
         Xtil = np.c_[np.ones(X.shape[0]), X]
         c = np.eye(Xtil.shape[1])
         A = np.dot(Xtil.T, Xtil) + self.lambda_ * c
         b = np.dot(Xtil.T, t)
-        self.w_ = linalg.solve(A, b)
+        self.w = linalg.solve(A, b)
 
     def predict(self, X):
-        Xtil = np.c_[np.ones()]
-        return np.dot(Xtil, self.w_)
+        b, a = self.w
+        return b + a * X
--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 from algorithms.ridge import Ridge
 from util.helper_functions import read_data, split_dataset
+from sklearn.model_selection import train_test_split
 from util.aqi import AQI
 
 def test_ridge():
@@ -11,13 +12,10 @@ def test_ridge():
     x = data["Vreme"].values
     x = np.linspace(0, len(x), len(x))
     y = data["CO [mg.m-3]"].ffill(axis=0).values
-    x_train, x_test, y_train, y_test = split_dataset(x,y)
-    model = Ridge(1.)
     model.fit(x, y)
-    b, a = model.w_
+    b, a = model.w
     plt.scatter(x, y, color="g")
-    xmax = x.max()
-    plt.plot([0, xmax], [b, b + a * xmax], color="k")
+    plt.plot([0, x.max()], [b, b + a * x.max()], color="k")
     plt.show()
 
 def test_aqi():
@@ -33,8 +31,23 @@ def test_aqi():
     print(aqi.get_value())
 
 
+def test_prediction():
+    data = read_data("beograd")
+    x = data["Vreme"].values
+    x = np.linspace(0, len(x), len(x))
+    y = data["CO [mg.m-3]"].ffill(axis=0).values
+    x_train, x_test, y_train, y_test = train_test_split(x,y)
+    model = Ridge(1.)
+    model.fit(x_train, y_train)
+    b, a = model.w
+    plt.scatter(x, y, color="g")
+    plt.plot([0, x_train.max()], [b, b + a * x_train.max()], color="k")
+    plt.plot([x_train[0], x_test.max()], [b, b + a * x_test.max()], color="r")
+    plt.show()
+
+
 def main():
-    test_aqi()
+    test_prediction()
 
 
 
