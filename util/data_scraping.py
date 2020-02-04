9bbe9165ff860c934a33440534d075837e54fbd0
Selich <selich.work@gmail.com>
2020-02-04 17:18:31 +0100

    :star:

--- util/data_scraping.py
+++ util/data_scraping.py
@@ -1,25 +1,7 @@
-# To add a new cell, type '#%%'
-# To add a new markdown cell, type '#%% [markdown]'
-#%%
-from IPython import get_ipython
-
-#%% [markdown]
-# # Predvidjanje kvaliteta vazduha u periodu od nedelju dana
-#%% [markdown]
-# ### Potrebne biblioteke:
-# - numpy
-#     - Koristimo za rad nad matricama
-# - pandas
-#     - Omogucava dobru organizaciju podataka i rad sa kljucevima i ostalim obelezijama
-# - requests
-#     - Sluzi za slanje http zahteva koji koristimo za pozivaje API-a
-# - selenium
-#     - Koristi se za automatsko koriscenje internet pretrazivaca
-# - BeautifulSoup
-#     - Omogucava citanje html stranica i izvacenje informacija iz iste
-
-#%%
 get_ipython().system('pip install --user beautifulsoup4 selenium numpy pandas ')
+from IPython import get_ipython
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 import numpy as np
 import pandas as pd
 import requests as req
@@ -28,52 +10,12 @@ import urllib.request
 import time
 import re
 
-#%% [markdown]
-# ### Poziv na API radi pribavljanja podataka
-# 
-# 
-# 
-
-#%%
-
-api_key = "c8055f9d-1fd0-4ad3-ab23-678ee0727b7b"
-base_url = "https://api.airvisual.com/v2"
-url_key = "?key=" + api_key
-url = base_url + "/countries" + url_key
-
-url_nearest_city = base_url + "/nearest_city" + url_key
-
-
-#%%
-data = req.get(url)
-nearest_city = req.get(url_nearest_city)
-
-
-#%%
-nearest_city.json()
-
-#%% [markdown]
-# ### Stavljanje podataka u Pandas dataframe
-# - Dobavljanje podataka za lokaciju Liman, Novi Sad sa sajta http://www.sepa.gov.rs/ za period od 30 dana.
-# 
-
-#%%
-
-
-#%% [markdown]
-# - Scraping podatke sa Sepa
-
-#%%
-from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
 amskv_url = "http://www.amskv.sepa.gov.rs/pregledpodatakazbirni.php"
 driver = webdriver.Chrome()
 driver.get(amskv_url)
-#%%https://yt3.ggpht.com/a-/AAuE7mAwo1CEpcecx08ZEX5QoWq7jQdSgg-CIx-Yqg=s900-mo-c-c0xffffffff-rj-k-no
 
 check_box = driver.find_element_by_xpath("/html/body/div[2]/div[2]/div/div[2]/form/div/div[1]/div[1]/div/label[10]/input")
 check_box.click()
-#%%
 
 komponente = [
     "/html/body/div[2]/div[2]/div/div[2]/form/div/div[1]/div[2]/div[1]/label[1]/input",
@@ -94,12 +36,10 @@ for k in komponente:
 
 prikazi = driver.find_element_by_xpath("/html/body/div[2]/div[2]/div/div[2]/form/div/div[1]/div[2]/a").click()
 
-#%%
 trideset_dana = driver.find_element_by_xpath("/html/body/div[2]/div[2]/div/div[2]/form/div/div[2]/div/div/div/div[1]/div/div[1]/label[3]").click()
 prikazi = driver.find_element_by_xpath("/html/body/div[2]/div[2]/div/div[2]/form/div/div[1]/div[2]/a").click()
 driver.find_element_by_xpath("/html/body/div[2]/div[2]/div/div[2]/form/div/div[2]/div/div/ul/li[2]/a").click()
 
-#%%
 NUMBER_OF_ROWS = 720
 
 def get_row_data(row,item):
@@ -131,24 +71,15 @@ def get_table_data(driver,df):
     return mat
 
 
-
-
-#%%
 labels = populate_with_labels(driver)
 
 df = pd.DataFrame(columns=labels)
 data = get_table_data(driver,df)
 labels
-#%%
+
 for i in range(0,NUMBER_OF_ROWS - 1):
     df.loc[i+1] = data[i]
 
-df.head()
-#%%
-
-
-
-#%%
 df.to_csv("./data/amskv_data.csv")
 
 
