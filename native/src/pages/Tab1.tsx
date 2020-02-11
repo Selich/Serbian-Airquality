fa4e62fbb667883b4cc0cd9ad47b23c226f981de
Selich <selich.work@gmail.com>
2020-02-11 01:48:10 +0100

    changes on native

--- native/src/pages/Tab1.tsx
+++ native/src/pages/Tab1.tsx
@@ -15,68 +15,73 @@ import {
   IonButton,
   IonTitle,
   IonToolbar
-} from '@ionic/react';
-import { book, build, colorFill, grid } from 'ionicons/icons';
-import React ,{ useState, useEffect } from 'react';
-import { images, flash, send } from 'ionicons/icons';
-import ProgressBar from '../components/CircularBar'
+} from "@ionic/react";
+import { book, build, colorFill, grid } from "ionicons/icons";
+import React, { useState, useEffect } from "react";
+import { images, flash, send } from "ionicons/icons";
+import ProgressBar from "../components/CircularBar";
 
+import "./Tab1.css";
 
-import './Tab1.css';
+import axios from "axios";
 
-import axios from 'axios'
-
-const fetchData = (url: string) => (
-    axios.get(url).then(res => {
-      console.log(res)
-      return res.data;
-    })
- )
+const fetchData = (url: string) =>
+  axios.get(url).then(res => {
+    console.log(res);
+    return res.data;
+  });
 
 // TODO: Color for AQ
-const colorArray = ['#7ea9e1', "#ed004f", "#00fcf0", "#d2fc00", "#7bff00", "#fa6900"];
+const colorArray = [
+  "#7ea9e1",
+  "#ed004f",
+  "#00fcf0",
+  "#d2fc00",
+  "#7bff00",
+  "#fa6900"
+];
 
 const Tab1: React.FunctionComponent = () => {
-
-  const [url, setUrl] = useState('')
-  const [pastData, setPastData] = useState({})
-  const [futureData, setFutureData] = useState({})
-  const [city, setCity] = useState('novisad')
-  const [color, setColor] = useState('')
+  const [url, setUrl] = useState("");
+  const [pastData, setPastData] = useState({});
+  const [futureData, setFutureData] = useState({});
+  const [city, setCity] = useState("novisad");
+  const [color, setColor] = useState("");
   const [progress, setProgress] = useState(0);
   const [currAQI, setCurrAQI] = useState(0);
 
 
   useEffect(() => {
-    axios.get('/aqi/' + city)
-    .then(res => setPastData(res.data))
-
+    axios.get("/aqi/" + city).then(res => setPastData(res.data));
   }, [city]);
 
   const randomColor = () => {
     return colorArray[Math.floor(Math.random() * colorArray.length)];
-  }
+  };
 
   const options = {
     size: 250,
     progress: currAQI,
     strokeWidth: 15,
-    circleOneStroke: '#d9edfe',
+    circleOneStroke: "#d9edfe",
     circleTwoStroke: colorArray[0]
-  }
+  };
 
+  const backgroundStyle = {
+    backgroundColor: "green",
+    fontSize: 22,
+    fontWeight: 900
+  };
 
   return (
-    <IonPage >
+    <IonPage>
       <IonHeader>
         <IonToolbar>
           <IonTitle>Novi Sad</IonTitle>
         </IonToolbar>
       </IonHeader>
-      <IonContent>
-        <div className="master">
-          <ProgressBar { ...options } />
-        </div>
+      <IonContent color="good">
+        <ProgressBar {...options} />
       </IonContent>
     </IonPage>
   );
