66b45e3c7db235ae4065335814772b00c6e46c6c
Selich <selich.work@gmail.com>
2020-02-11 01:14:51 +0100

    circular bar done

--- native/src/pages/Tab1.tsx
+++ native/src/pages/Tab1.tsx
@@ -19,6 +19,8 @@ import {
 import { book, build, colorFill, grid } from 'ionicons/icons';
 import React ,{ useState, useEffect } from 'react';
 import { images, flash, send } from 'ionicons/icons';
+import ProgressBar from '../components/CircularBar'
+
 
 import './Tab1.css';
 
@@ -31,34 +33,49 @@ const fetchData = (url: string) => (
     })
  )
 
+// TODO: Color for AQ
+const colorArray = ['#7ea9e1', "#ed004f", "#00fcf0", "#d2fc00", "#7bff00", "#fa6900"];
+
 const Tab1: React.FunctionComponent = () => {
 
   const [url, setUrl] = useState('')
-  const [pastData, setPastData] = useState([])
-  const [futureData, setFutureData] = useState([])
+  const [pastData, setPastData] = useState({})
+  const [futureData, setFutureData] = useState({})
   const [city, setCity] = useState('novisad')
+  const [color, setColor] = useState('')
+  const [progress, setProgress] = useState(0);
+  const [currAQI, setCurrAQI] = useState(0);
 
 
   useEffect(() => {
     axios.get('/aqi/' + city)
     .then(res => setPastData(res.data))
+
   }, [city]);
+
+  const randomColor = () => {
+    return colorArray[Math.floor(Math.random() * colorArray.length)];
+  }
+
+  const options = {
+    size: 250,
+    progress: currAQI,
+    strokeWidth: 15,
+    circleOneStroke: '#d9edfe',
+    circleTwoStroke: colorArray[0]
+  }
 
 
   return (
     <IonPage >
       <IonHeader>
         <IonToolbar>
-          <IonTitle>{city}</IonTitle>
+          <IonTitle>Novi Sad</IonTitle>
         </IonToolbar>
       </IonHeader>
       <IonContent>
         <div className="master">
-          { pastData.forEach(row => {console.log(row);
-          })
-          }
-        <IonCardTitle className="aqi-score">45</IonCardTitle>
-        <IonCardSubtitle className="aqi-city">Novi Sad</IonCardSubtitle>
+          <ProgressBar { ...options } />
         </div>
       </IonContent>
     </IonPage>
